35abb5c8026a3461ef8edc887bcb1a9e917c5af1
rodrigolima131 <rodrigo.lima131@gmail.com>
2019-07-01 16:24:19 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ ProjetoTeste/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo");
+            Console.ReadLine();        }
+    }
+}
